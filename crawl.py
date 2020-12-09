7fe8163d42fada2def2a6e8ae38f22b19212ddd5
raphaelseo22 <seodh02@gmail.com>
2020-12-09 11:30:52 +0900

    20201209

--- crawl.py
+++ crawl.py
@@ -12,21 +12,22 @@ def get_dom(url, encode='utf-8'):
     return soup
 
 
-def selector(url, category, select):
+def selector(url, select):
     soup = get_dom(url)
     select_ls = []
     for ti in soup.select(select):
         select_ls.append(ti.text.strip())
-    table = pd.DataFrame()
     if len(select_ls) > 20:
-        select_ls = select_ls[3:]
-    table[f'{category}'] = select_ls
-    return table
+        while len(select_ls) != 20:
+            select_ls = select_ls[1:]
+    return select_ls
 
 
 def crawl(url, title, time):
-    title_table = selector(url, 'title',title)
-    time_talbe = selector(url, 'time',time)
+    title_ls = selector(url, title)
+    time_ls = selector(url, time)
     # address_talbe = selector(url, address)
-    total = pd.concat([title_table, time_talbe], ignore_index=True)
-    return total
+    table = pd.DataFrame()
+    table['Title'] = title_ls
+    table['Time'] = time_ls
+    return table
