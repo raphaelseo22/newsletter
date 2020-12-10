8aa63d81da5a3ff5561a49797fb77a87928a430c
raphaelseo22 <seodh02@gmail.com>
2020-12-10 14:03:05 +0900

    almost

--- scheduler.py
+++ scheduler.py
@@ -69,7 +69,7 @@ sched = BackgroundScheduler()
 
 
 sched.start()
-sched.add_job(newsletter, 'interval', minutes=1)
+sched.add_job(newsletter, 'cron', hour="6,18")
 while True:
     print("Running main process...............")
-    time.sleep(60)
+    time.sleep(60*60)
